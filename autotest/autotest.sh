292cf93c3fea6b9e5aed9a99ddc14989a1ddad1b
seigot <you@example.com>
2021-02-11 12:05:41 +0900

    disable capture function in autotest.sh

--- autotest/autotest.sh
+++ autotest/autotest.sh
@@ -27,7 +27,7 @@ function do_game(){
     # change directory
     pushd ${BURGER_WAR_KIT_REPOSITORY}
 
-    bash scripts/capture.sh -m "start"
+    #bash scripts/capture.sh -m "start"
 
     # wakeup gazebo/judgeserver
     PROCESS_NUM=`ps -ux | grep "sim_with_judge.sh" | grep -v "grep"  | wc -l`
@@ -67,7 +67,7 @@ function do_game(){
     VIDEO_DIRECTORY_PATH="${HOME}/video/${TODAY}/"
     mkdir -p ${VIDEO_DIRECTORY_PATH}
     VIDEO_NAME="${VIDEO_DIRECTORY_PATH}/"GAME_${DATE}_${ITERATION}_${ENEMY_LEVEL}_${GAME_TIME}_${MY_SCORE}_${ENEMY_SCORE}_${BATTLE_RESULT}_${MY_SIDE}".mp4"
-    bash scripts/capture.sh -m "stop" -n ${VIDEO_NAME}
+    #bash scripts/capture.sh -m "stop" -n ${VIDEO_NAME}
 
     ## reset
     #bash scripts/reset.sh
