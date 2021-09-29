6de3cd3a94d408156b4a3e203747e9b594ded92e
seigot <46782860+seigot@users.noreply.github.com>
2021-09-30 00:25:42 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -18,7 +18,7 @@ bash judge/test_scripts/set_running.sh localhost:5000
 
 # launch robot control node
 declare -a ROSLAUNCH_OPTIONS=("enemy_level:=${VALUE_L}")
-if [ "${IS_AUTOTEST}" = "TRUE" ]; then
-    ROSLAUNCH_OPTIONS+=("rviz_file:=burger_navigation_autotest.rviz")
-fi
+#if [ "${IS_AUTOTEST}" = "TRUE" ]; then
+#    ROSLAUNCH_OPTIONS+=("rviz_file:=burger_navigation_autotest.rviz")
+#fi
 roslaunch burger_war sim_robot_run.launch ${ROSLAUNCH_OPTIONS[@]}
